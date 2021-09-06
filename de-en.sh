919389210af8daffbc52c102523b71425e415fcf
Wilker Aziz <wilker.aziz@gmail.com>
2021-09-06 12:55:20 +0100

    typo

--- de-en.sh
+++ de-en.sh
@@ -1,6 +1,6 @@
 mkdir -p models/fairseq
 echo "Downloading de-en"
-!wget -O models/fairseq/de-en.tar https://surfdrive.surf.nl/files/index.php/s/8lnmJPX3iXndRpz/download
+wget -O models/fairseq/de-en.tar https://surfdrive.surf.nl/files/index.php/s/8lnmJPX3iXndRpz/download
 echo "Uncompressing"
 tar -xvf models/fairseq/de-en.tar -C models/fairseq/
 echo "Check: models/fairseq/de-en"
