39f1987493ee46f6b8e732aa018c8b97b4300188
Wilker Aziz <wilker.aziz@gmail.com>
2021-09-06 15:04:46 +0100

    test before download

--- en-de.sh
+++ en-de.sh
@@ -1,6 +1,9 @@
-mkdir -p models/fairseq
-echo "Downloading en-de"
-wget -O models/fairseq/en-de.tar https://surfdrive.surf.nl/files/index.php/s/TsTcy5oCXFjpmTo/download
-echo "Uncompressing"
-tar -xvf models/fairseq/en-de.tar -C models/fairseq/
-echo "Check: models/fairseq/en-de"
+if [[ ! -f models/fairseq/en-de/mle/averaged_model.pt ]]; then
+  mkdir -p models/fairseq
+  echo "Downloading en-de"
+  wget -O models/fairseq/en-de.tar https://surfdrive.surf.nl/files/index.php/s/TsTcy5oCXFjpmTo/download
+  echo "Uncompressing"
+  tar -xvf models/fairseq/en-de.tar -C models/fairseq/
+  rm models/fairseq/en-de.tar
+  echo "Check: models/fairseq/en-de"
+fi
